87a79c12154a210ed7b18869f1ef299baa1409cd
KCYLion <1095756998@qq.com>
2022-03-06 09:21:59 +0800

    Update rclone.sh

--- config/script/rclone.sh
+++ config/script/rclone.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ "${RCLONE_CONFIG_BASE64}" != ]]; then
+if [ "${RCLONE_CONFIG_BASE64}" != ]; then
   echo "[INFO] Config Rclone from RCLONE_CONFIG_BASE64 env"
   echo $RCLONE_CONFIG_BASE64 | base64 -d > /config/rclone.conf
   echo "[INFO] Config Rclone from RCLONE_CONFIG_BASE64 completed"
