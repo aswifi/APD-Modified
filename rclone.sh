24bc5dff96491c770fc95ca77bf1a37ed33e8a64
KCYLion <1095756998@qq.com>
2022-03-06 09:44:21 +0800

    Update rclone.sh

--- rclone.sh
+++ rclone.sh
@@ -2,6 +2,6 @@
 
 if [ "$RCLONE_CONFIG_BASE64" != "" ]; then
   echo "[INFO] Config Rclone from RCLONE_CONFIG_BASE64 env"
-  echo $RCLONE_CONFIG_BASE64 | base64 -d > /app/conf/rclone.conf
+  echo $RCLONE_CONFIG_BASE64 | base64 -d > /config/rclone.conf
   echo "[INFO] Config Rclone from RCLONE_CONFIG_BASE64 completed"
 fi
